0a68c619943c936d1dc4c26a3884ad46680c55d4
christmas1409 <55200821+christmas1409@users.noreply.github.com>
2022-08-19 10:30:06 +0300

    Реализация задачи

---
+++ TextMessengerAPI.Web/Features/GetIDRGDialogQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextMessengerAPI.Web.Features
+{
+    public class GetIDRGDialogQuery
+    {
+        public IEnumerable<Guid> ClientIds { get; set; }
+    }
+}
